4657f183d4d500e8ebe9d0b0d4544b2de468c224
Alexander <hagriel@gmail.com>
2021-03-04 12:26:00 +0200

    dd

---
+++ folder/folder2/folder3/index.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line import/no-unresolved
+const sql = require('mssql');
+const password = 'sdadsdas';
+
+exports.handler = async (event, context, callback) => {
+  const config = {
+    user: 'admin',
+    password: '',
+    server: '',
+    database: 'SALTO_SPACE',
+  };
+
+  try {
+    const d = new Date();
+    let dateString = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+    dateString += `${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}`;
+    const connection = await sql.connect(config);
+    const q = `exec msdb.dbo.rds_backup_database
+    @source_db_name='${config.database}',
+    @s3_arn_to_backup_to='arn:aws:s3:::goldprop-dbbackup/salto:${dateString}.sql.but',
+    @overwrite_s3_backup_file=0,
+    @type='FULL'`;
+    const result = await connection.request()
+      .query(q);
+    // eslint-disable-next-line no-console
+    console.log('Backup result', result);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+
+  sql.on('error', (err) => {
+    // eslint-disable-next-line no-console
+    console.log(err);
+    callback(err);
+  });
+};
